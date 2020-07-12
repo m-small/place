d1b4b1f1e02f42fcaa1779dd933c80f6c22ef690
Michael Small <michael.small@uwa.edu.au>
2020-07-12 14:45:58 +0800

    Everything works, but not very well

--- Place.jl
+++ Place.jl
@@ -1,6 +1,6 @@
 module Place
 
-	export buildmodel, placebo, predict, freerun, BasisFunc, PlaceModel
+	export buildmodel, placebo, predict, freerun, description, BasisFunc, PlaceModel
 
 	using ToeplitzMatrices, Random, StatsBase, Distributions, LinearAlgebra
 
@@ -46,16 +46,20 @@ module Place
 			npts=length(yt)
 		end
 		Xi=X[:,i]
-		Xp=Array{Float64,2}(undef,dX,npts+1)
+		yt=yt[i:end]
+		Xp=Array{Float64,2}(undef,dX,npts)
 		for j in 1:npts
 			ϕ, offset = placebo(Xi[:,:], model.rbf) #Xi needs to be 2-D!
 			yp=ϕ[:,model.basis]*model.λ
-			Xi=[yp; Xi[1:(end-1)]]
+			Xi=[yp; Xi[1:end-1]]     #future is at the TOP!!!!
 			Xp[:,j]=Xi
 		end
 		yp=Xp[1,:]
+		if length(yt)>npts
+			yt=yt[1:npts]
+		end
 		#
-		return yp
+		return yp, yt
 	end
 
 
@@ -92,21 +96,26 @@ module Place
 		if "penalty"∈keys(options)
 			penalty=options["penalty"]
 		else
-			penalty = :(dx*log(mss)+2*nk)  	#Akaike Information Criterion
-		#	penalty = :(dx*log(mss)+nk*log(dx))	#Schwarz Information Crtierion
+			penalty = :(nx*log(mss)+2*nk)  	#Akaike Information Criterion
+		#	penalty = :(nx*log(mss)+nk*log(nx))	#Schwarz Information Crtierion
+			penalty = :description(mss,λ,δ,nx) #Rissanen desciption length
 		end
 		if "functions"∈keys(options)
 			functype=options["functions"]
 		else
 			functype=(gaussian, tophat)
 		end
-
+		if "nneighbours"∈keys(options)
+			nneighbours=options["nneighbours"]
+		else
+			nneighbours=1
+		end
 
 		#embed the data
 		X,z,Xp,zp = embedding(y,v,td)
 		
 		#generate some basis functions
-		rbfset = getbasis(X,z,functype,v, nbasis)
+		rbfset = getbasis(X,z,functype,v, nbasis, nneighbours)
 
 		#choose basis functions
 		rbf,λ,basis,mdlv = topdown(X,z,rbfset,penalty,stopstep)
@@ -120,11 +129,12 @@ module Place
 
 	function embedding(y,v,td=0)
 		#embed y with strategy adequate for v
+		#remember - future is at the TOP!
 		de=maximum([maximum(vv) for vv in v])+1 #maximum embedding lag required
 		x0=y[1:(de+1)] #initial condition
 		z=y[(de+1):end] #predicted target
 		X=ToeplitzMatrices.Hankel(x0,z) #sparse embedded matrix
-		X=X[1:end-1,:] #state matrix
+		X=X[end-1:-1:1,:] #state matrix flip it
 		(de,nX)=size(X)
 		if isempty(td)
 			td=Int(floor(nX/2))
@@ -146,23 +156,63 @@ module Place
 		return X, z, Xp, zp
 	end
 
-	function getbasis(X :: Array{Float64,2}, y :: Array{Float64, 1}, functions, v, nbasis) :: Array{BasisFunc,1}
+	function nearestofnneighbours(X::Array{Float64,2},randi::Array{Int64,1},y,n::Int64=1)
+		#distances to the closest of n randomly choosen neighbours - multiple reference points
+		dx,nx =size(X)
+		ny=length(y)
+		nbasis=length(randi)
+		dd=Array{Float64,1}(undef,nbasis)
+		dd .= Inf
+		for i in 1:n
+			randomneighbour=sample(1:ny, Weights(abs.(y)),nbasis) 
+			randr=X[:,randi]-X[:,randomneighbour]
+			randr=normn(randr)
+			dd=minimum([dd'; randr], dims=1)'
+		end
+		return dd
+	end
+
+	function nearestofnneighbours(X::Array{Float64,2},randi::Int64,y,n::Int64=1)
+		#distances to the closest of n randomly choosen neighbours - single reference point
+		dx = length(X)
+		ny = length(y)
+		randomneighbour=sample(1:ny, Weights(abs.(y)),n) 
+		randr=X[:,randi]*ones(1,n) - X[:,randomneighbour]
+		randr=normn(randr)
+		dd = minimum(randr)
+		return dd
+	end
+
+	function normn(X,n:: Int64=2)
+		#n-norm over columns of a square array X
+		#
+		return (sum(X.^n, dims=1)).^(1/n)
+	end
+
+	function getbasis(X :: Array{Float64,2}, y :: Array{Float64, 1}, functions, v, nbasis, nnearest) :: Array{BasisFunc,1}
 		#generate a random set of basis functions, distributed over the data X according to pdf (X,y)
 		de,nx=size(X)
 		rbfs=Array{BasisFunc,1}(undef,nbasis)
-		randfunct=rand(functions,nbasis)
+		randfunct=Array{Any,1}(undef,nbasis)
+		if functions isa Tuple
+			randfunct=rand(functions,nbasis)
+		else
+			randfunct.=functions
+		end
 		randi=sample(1:nx, Weights(abs.(y)),nbasis)
-		randomneighbour=sample(1:nx, Weights(abs.(y)),nbasis) 
+	#	randomneighbour=sample(1:nx, Weights(abs.(y)),nbasis) 
 		randr=Array{Float64,1}(undef,nbasis)
 		if v isa Tuple
 			randv=rand(v,nbasis)
 			for i in 1:nbasis
-				randr[i]=normn(X[randv[i].+1,randi[i]]-X[randv[i].+1,randomneighbour[i]])[1]
+				#randr[i]=normn(X[randv[i].+1,randi[i]]-X[randv[i].+1,randomneighbour[i]])[1]
+				randr[i]=nearestofnneighbours(X[randv[i].+1,:],randi[i],y,nnearest)
 			end
 		else
 			randv=[]
-			randr=X[v.+1,randi]-X[v.+1,randomneighbour]
-			randr=normn(randr)
+			#randr=X[v.+1,randi]-X[v.+1,randomneighbour]
+			#randr=normn(randr)
+			randr=nearestofnneighbours(X[v.+1,:],randi,y,nnearest)
 		end
 		randr=abs.(randr.*randn(nbasis))
 		for i in 1:nbasis
@@ -191,21 +241,59 @@ module Place
 		return rbfs
 	end
 
-	function normn(X,n:: Int64=2)
-		#n-norm over columns of a square array X
-		#
-		return (sum(X.^n, dims=1)).^(1/n)
+	function finddeltas(Q,δ)
+		#compute solution of Qδ=1/δ from initial guess δ
+		#via Newtons method
+	#	print(size(Q))
+	#	println(" in finddeltas")
+		nq,mq=size(Q) # should be the same and the same as length(δ)
+		if nq==1 #solveable for dimension 1
+			δ=sqrt(Q)
+		end #otherwise...
+	#	println(size(Q))
+		return δ
+	end
+
+	function lstar(λ,δ) # λ::Float64,δ::Float64 ?
+		#Compute universal prior encoding of positive float λ to precision δ
+        λ=abs.(λ)
+		δ=abs.(δ)
+	#	println("In lstar: λ=$λ, δ=$δ")
+		bigl1 = lstar(2*ceil.(λ ./ δ))
+		bigl2 = lstar(ceil.(abs.(2*log.(map(x->max(x,1/x),λ)))))
+	#	println("Output: $bigl1+$bigl2=$(bigl1+bigl2)")
+		return bigl1+bigl2
+	end
+
+	function lstar(λ) # λ::Int64 ?
+		#Compute universal prior encoding of positive integers λ
+        λ=abs.(λ)
+		bigl=log(2.865)
+		λ=1 .+ ceil.(λ)
+		while any(x-> x>1, λ)
+			λ=log.(map(x->max(x,1),λ))
+			bigl=bigl .+ λ
+		end
+		return bigl
+	end
+
+	function description(mss,λ,δ,nx)
+		#Compute description length encoding (ala Rissanen) of model with 
+		#error mss, parameters λ to precision δ and nx data
+		global needδ=true
+		return 0.5*nx*(1+log(2*π*mss)) + sum(lstar(λ,δ))
 	end
 
 	function topdown(X,y,allrbfs:: Array{BasisFunc,1},penalty::Expr,stopstep::Int64)
 		#select an optimal set of basis functions to fit X to y.
-		global mss, nx, nk   #needed for penalty function evaluation
+		global needδ, mss, nx, nk, λ, δ   #needed for penalty function evaluation
 		ϕ,offset=placebo(X,allrbfs)
 		nb=length(allrbfs)
 		modelbasis=[]
 		ϕQ=Array{Float64,2}(undef,0,0)
 		ϕR=Array{Float64,2}(undef,0,0)
-#		δ=Array{Float64,2}(undef,0,0)
+		λ=Array{Float64,1}(undef,0)
+		δ=Array{Float64,1}(undef,0)
 		err=y
 		mss=mean(y'*y)
 		ny=length(y)
@@ -216,6 +304,9 @@ module Place
 		mdlv=[]
 		notimproved=0
 		bestmodelbasis=modelbasis
+		needδ=false
+		eval(penalty) #just to set needδ=true if necessary...
+		println("needδ=$needδ")
 		while nk<nb && notimproved<stopstep
 			#
 			μ = -ϕ'*err #compute sensitivity
@@ -223,23 +314,39 @@ module Place
 			~,ind = findmax(abs.(μ))
 			append!(modelbasis, ind)
 			ϕQ,ϕR = qrappend(ϕQ,ϕR,ϕ[:,ind])
-			a1 = ϕR\(ϕQ'*y) #new model weights
+			λ = ϕR\(ϕQ'*y) #new model weights
+			if needδ
+				if isempty(δ)
+					δ=sqrt(mean(λ))
+				else
+					δ=δ[:] #needs a vector not an 1-by-n matrix - damn you Julia
+					append!(δ, mean(δ)) 
+				end
+			end
+
 			#now delete
-			~,ind = findmin(abs.(a1))
+			~,ind = findmin(abs.(λ))
 			if ind==(nk+1) #last added is worst so keep it
-	#			append!(δ,mean(δ))
 				nk += 1
 			else
 				#last added is not worst, so delete new worst
 				deleteat!(modelbasis,ind)
 				ϕQ,ϕR = qrdelete(ϕQ,ϕR,ind)
+				if needδ
+					δ=δ[:] #needs a vector not an 1-by-n matrix - damn you Julia
+					deleteat!(δ,ind)
+				end
+			end
+			#
+			if needδ
+				Q = ϕQ'*ϕQ/mss
+				δ = finddeltas(Q,δ)
 			end
 			#   
-			a1 = ϕR\(ϕQ'*y) #new model weights   
-			err = y - ϕ[:,modelbasis]*a1
+			λ = ϕR\(ϕQ'*y) #new model weights   
+			err = y - ϕ[:,modelbasis]*λ
          	mss = (err'*err/ny)
          	dl=eval(penalty)
-         	#dl=(dx*log(msserr)+nk*log(dx))
          	if dl<mdl #is this model best?
          		mdl=dl
          		bestmodelbasis=deepcopy(modelbasis)
@@ -249,7 +356,11 @@ module Place
          		notimproved += 1
          	end
          	println("MSS=$mss DL=$dl size=$nk")
-         	append!(mdlv,dl)
+         	if nk>length(mdlv)
+         		append!(mdlv,dl)
+         	else
+         		mdlv[nk]=dl
+         	end
 		end
 		#λ = ϕR\(ϕQ'*y) 									
 		modelbasis=bestmodelbasis

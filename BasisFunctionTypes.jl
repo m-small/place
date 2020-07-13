45f1ac991c124a427e83717423e3c9eb665baa14
Michael Small <michael.small@uwa.edu.au>
2020-07-13 17:08:36 +0800

    more functions

--- BasisFunctionTypes.jl
+++ BasisFunctionTypes.jl
@@ -1,10 +1,10 @@
-function tophat(x,p)
+function tophat(x,p)   #first function evaluates the vector input x at parameter p
     return exp.(-x.^p)
 end
-function dtophat(x,p)
+function dtophat(x,p)  #second function is the derivative
 	return -p* (x.^(p-1)) .* exp.(-x.^p)
 end
-function tophat()
+function tophat() #third function, with no input arguments, gives the default range of parameters p
 	return [1.1 5;]
 end
 
@@ -27,3 +27,48 @@ end
 function dcubic(x)
 	return 3 .* x.^2
 end
+
+function quintic(x)
+	return x.^5
+end
+function quintic()
+	return []
+end
+function dquintic(x)
+	return 5 .* x.^4
+end
+
+function wavelet(x)
+	return (2*x.^2-1) .* exp.(-x.^2)
+end
+function wavetlet()
+	return []
+end
+function dwavelet(x)
+	return (2*x.*(3-2*x.^2)) .* exp.(-x.^2)
+end
+
+function sigmoid(x)
+	return tanh.( x )
+end
+function sigmoid()
+	return []
+end
+function dsigmoid(x)
+	return (1-tanh.( x ).^2)
+end
+
+function morlet(x,p)
+	return cos.(2*pi*x) .* exp.(-(2*(pi/p)^2)*x.^2) .- exp.(-p^2/2-(2*(pi/p)^2)*x.^2)
+end
+function morlet()
+	return [1 9;]
+end
+function dmorlet(x,p)
+	return -2*(pi.*sin(2*pi*x) .+ 2*((pi/p)^2)*x.*cos.(2*pi*x)) .* exp(-(2*(pi/p)^2)*x.^2) .+ (4*((pi/p)^2)*x) .* exp.(-p^2/2-(2*(pi/p)^2)*x.^2)
+end
+
+
+
+
+
